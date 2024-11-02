106b893e137614c7c5c19a6132e74abd19456c1f
Miguel Moreno <miguelmorenov26@gmail.com>
2024-11-01 22:17:38 -0600

    alerts created

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-import express from 'express';
+import express from 'express'
+import config from './config.js'
+import colors from 'colors'
 
 const app = express();
 
-app
+// // Iniciar servidor
+
+app.listen(config.PORT,()=>{
+  console.log(`Server `.green + `El servicio esta funcionando en \n http direction: ` + `http://localhost:${config.PORT}`.blue)
+
+})
+
